aa4640c9f9b80ae7383623fac89c3629f46482f0
AWS Server <668jerry@gmail.com>
2014-10-29 22:56:04 +0800

    button added

--- 1129vday/view/FREEViewController.m
+++ 1129vday/view/FREEViewController.m
@@ -64,10 +64,69 @@
     [self.view addSubview:uitvContent];
 }
 
+- (void)uibClickedTsai {
+    
+}
+
+- (void)setButton {
+    UIButton *uibTsai = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    uibTsai.frame = CGRectMake(cgfAvailableWidth * 0.06, cgfAvailableHeight * 0.09 + cgfAvailableHeightStart, cgfAvailableWidth * 0.78, cgfAvailableWidth * 0.88 * 154.0 / 1315.0);
+    [uibTsai addTarget:self action:@selector(uibClickedTsai) forControlEvents:UIControlEventTouchUpInside];
+    uibTsai.tintColor = [UIColor blackColor];
+    [uibTsai setTitle:@"蔡正元選區：正元手術房" forState:UIControlStateNormal];
+    [self.view addSubview:uibTsai];
+    
+    UIButton *uibWu = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    uibWu.frame = CGRectMake(cgfAvailableWidth * 0.06, cgfAvailableHeight * 0.12 + cgfAvailableHeightStart, cgfAvailableWidth * 0.78, cgfAvailableWidth * 0.88 * 154.0 / 1315.0);
+    [uibWu addTarget:self action:@selector(uibClickedTsai) forControlEvents:UIControlEventTouchUpInside];
+    uibWu.tintColor = [UIColor blackColor];
+    [uibWu setTitle:@"吳育昇選區：海口夯社" forState:UIControlStateNormal];
+    [self.view addSubview:uibWu];
+    
+    UIButton *uibLin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    uibLin.frame = CGRectMake(cgfAvailableWidth * 0.06, cgfAvailableHeight * 0.15 + cgfAvailableHeightStart, cgfAvailableWidth * 0.78, cgfAvailableWidth * 0.88 * 154.0 / 1315.0);
+    [uibLin addTarget:self action:@selector(uibClickedTsai) forControlEvents:UIControlEventTouchUpInside];
+    uibLin.tintColor = [UIColor blackColor];
+    [uibLin setTitle:@"吳育昇選區：海口夯社" forState:UIControlStateNormal];
+    [self.view addSubview:uibLin];
+
+    UIButton *uibDragon = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    uibDragon.frame = CGRectMake(cgfAvailableWidth * 0.06, cgfAvailableHeight * 0.29 + cgfAvailableHeightStart, cgfAvailableWidth * 0.78, cgfAvailableWidth * 0.88 * 154.0 / 1315.0);
+    [uibDragon addTarget:self action:@selector(uibClickedTsai) forControlEvents:UIControlEventTouchUpInside];
+    uibDragon.tintColor = [UIColor blackColor];
+    [uibDragon setTitle:@"蔡錦龍選區" forState:UIControlStateNormal];
+    [self.view addSubview:uibDragon];
+
+    UIButton *uibHuang = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    uibHuang.frame = CGRectMake(cgfAvailableWidth * 0.06, cgfAvailableHeight * 0.56 + cgfAvailableHeightStart, cgfAvailableWidth * 0.78, cgfAvailableWidth * 0.88 * 154.0 / 1315.0);
+    [uibHuang addTarget:self action:@selector(uibClickedTsai) forControlEvents:UIControlEventTouchUpInside];
+    uibHuang.tintColor = [UIColor blackColor];
+    [uibHuang setTitle:@"黃昭順選區" forState:UIControlStateNormal];
+    [self.view addSubview:uibHuang];
+
+    UIButton *uibCountry = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    uibCountry.frame = CGRectMake(cgfAvailableWidth * 0.06, cgfAvailableHeight * 0.59 + cgfAvailableHeightStart, cgfAvailableWidth * 0.78, cgfAvailableWidth * 0.88 * 154.0 / 1315.0);
+    [uibCountry addTarget:self action:@selector(uibClickedTsai) forControlEvents:UIControlEventTouchUpInside];
+    uibCountry.tintColor = [UIColor blackColor];
+    [uibCountry setTitle:@"林國正選區" forState:UIControlStateNormal];
+    [self.view addSubview:uibCountry];
+
+//    UIButton *uibLin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+//    uibLin.frame = CGRectMake(cgfAvailableWidth * 0.698, cgfAvailableHeight * 0.408 + cgfAvailableHeightStart, cgfAvailableWidth * 0.13, cgfAvailableWidth * 0.13 * 173.0 / 176.0);
+//    [uibLin addTarget:self action:@selector(uibClickedLin) forControlEvents:UIControlEventTouchUpInside];
+//    uibLin.tintColor = [UIColor colorWithRed:0.71 green:0.13 blue:0.25 alpha:1.0];
+//    [self.view addSubview:uibLin];
+    
+//    UIImage *uiiGo = [UIImage imageNamed:@"supp_go"];
+//    [uibTsai setImage:uiiGo forState:UIControlStateNormal];
+//    [uibWu setImage:uiiGo forState:UIControlStateNormal];
+//    [uibLin setImage:uiiGo forState:UIControlStateNormal];
+}
+
 - (void)drawScreen {
     [self setMyScreenSize];
     [self setImageAndLabel];
-    //    [self setButton];
+    [self setButton];
 }
 
 - (void)viewDidLoad {
