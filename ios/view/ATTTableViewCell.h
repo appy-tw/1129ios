6731a0b7aba28481caba57eb00f4beecf4615a67
AWS Server <668jerry@gmail.com>
2014-11-06 12:47:34 +0800

    localizable and all table view controllers

---
+++ ios/view/ATTTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ATTTableViewCell.h
+//  ios
+//
+//  Created by lololol on 6/Nov/14.
+//  Copyright (c) 2014 Appendectomy Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATTTableViewCell : UITableViewCell
+
+@end
---
+++ ios/view/ATTTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ATTTableViewCell.m
+//  ios
+//
+//  Created by lololol on 6/Nov/14.
+//  Copyright (c) 2014 Appendectomy Project. All rights reserved.
+//
+
+#import "ATTTableViewCell.h"
+
+@implementation ATTTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
