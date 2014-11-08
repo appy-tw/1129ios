db717a4e36a4fa9908e595140e2978c3a93788e7
AWS Server <668jerry@gmail.com>
2014-11-08 16:36:22 +0800

    fb name

--- ios/view/ATTTableViewController.m
+++ ios/view/ATTTableViewController.m
@@ -13,7 +13,7 @@
 #import <Parse/Parse.h>
 
 
-@interface ATTTableViewController ()
+@interface ATTTableViewController () <FBLoginViewDelegate>
 {
     CGFloat cgfW;
     
@@ -34,6 +34,9 @@
     UIImage* uiiATT3;
     UIImage* uiiATT4;
     UIImage* uiiATT6;
+    
+    FBLoginView *fbLoginView;
+    UILabel *uilFBUserName;
 }
 @end
 
@@ -65,10 +68,31 @@
     uiiATT6 = [UIImage imageNamed:@"att6"];
 }
 
+- (void)setFBView {
+    uilFBUserName = [[UILabel alloc]initWithFrame:CGRectMake(cgfScreenWidth * 0.58, 410.0 * cgfScreenWidth / 640.0 * 0.85, cgfScreenWidth * 0.40, 30.0)];
+    fbLoginView = [[FBLoginView alloc] initWithReadPermissions:@[@"public_profile", @"email", @"user_friends"]];
+    
+    fbLoginView.frame = CGRectOffset(fbLoginView.frame, cgfScreenWidth / 12.0, 0);
+#ifdef __IPHONE_7_0
+#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
+    if ([self respondsToSelector:@selector(setEdgesForExtendedLayout:)]) {
+        fbLoginView.frame = CGRectOffset(fbLoginView.frame, cgfScreenWidth / 12.0, 0);
+    }
+#endif
+#endif
+#endif
+    fbLoginView.delegate = self;
+    
+    [self.view addSubview:fbLoginView];
+    [fbLoginView sizeToFit];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setImage];
     [self setMyScreen];
+    [self setFBView];
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
 }
 
@@ -77,7 +101,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 8;
+    return 9;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -105,7 +129,18 @@
         UIImageView *uiimv = [[UIImageView alloc]initWithFrame:CGRectMake(0.0, 0.0, self.tableView.frame.size.width, self.tableView.frame.size.width * 410 / 640)];
         uiimv.image = uiiATT2;
         [cell.contentView addSubview:uiimv];
+        [uilFBUserName setTextAlignment:NSTextAlignmentCenter];
+        [cell.contentView addSubview:uilFBUserName];
     } else if (indexPath.row == 2) {
+        cell = [tableView dequeueReusableCellWithIdentifier:nssIDATT2];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nssIDATT2];
+        }
+        //    loginView.center = self.view.center;
+        //    [self.view addSubview:loginView];
+        fbLoginView.center = cell.contentView.center;
+        [cell.contentView addSubview:fbLoginView];
+    } else if (indexPath.row == 3) {
         cell = [tableView dequeueReusableCellWithIdentifier:nssIDATT2];
         if (cell == nil) {
             cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nssIDATT2];
@@ -113,7 +148,7 @@
         UIImageView *uiimv = [[UIImageView alloc]initWithFrame:CGRectMake(self.tableView.frame.size.width * 40 / 640, 0.0, self.tableView.frame.size.width * 83 / 640, self.tableView.frame.size.width * 95 / 640)];
         uiimv.image = uiiATT3;
         [cell.contentView addSubview:uiimv];
-    } else if (indexPath.row == 3) {
+    } else if (indexPath.row == 4) {
         cell = [tableView dequeueReusableCellWithIdentifier:nssIDATT4];
         if (cell == nil) {
             cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nssIDATT4];
@@ -121,7 +156,7 @@
         UIImageView *uiimv = [[UIImageView alloc]initWithFrame:CGRectMake(self.tableView.frame.size.width * 0.2867, 0.0, self.tableView.frame.size.width * 273.0 / 640.0, self.tableView.frame.size.width * 78 / 640)];
         uiimv.image = uiiATT4;
         [cell.contentView addSubview:uiimv];
-    } else if (indexPath.row == 4) {
+    } else if (indexPath.row == 5) {
         cell = [tableView dequeueReusableCellWithIdentifier:nssIDATT2];
         if (cell == nil) {
             cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nssIDATT2];
@@ -150,14 +185,102 @@
     } else if (indexPath.row == 2) {
         return self.tableView.frame.size.width * 95 / 640 + 20.0;
     } else if (indexPath.row == 3) {
-        return self.tableView.frame.size.width * 78 / 640 + 20.0;
-    } else if (indexPath.row == 4) {
         return self.tableView.frame.size.width * 95 / 640 + 20.0;
+    } else if (indexPath.row == 4) {
+        return self.tableView.frame.size.width * 78 / 640 + 20.0;
     } else if (indexPath.row == 5) {
+        return self.tableView.frame.size.width * 95 / 640 + 20.0;
+    } else if (indexPath.row == 6) {
         return self.tableView.frame.size.width * 112 / 640 + 20.0;
     } else {
         return self.tableView.frame.size.width * 300 / 640 + 20.0;
     }
 }
+
+#pragma mark - FBLoginViewDelegate
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+    // first get the buttons set for login mode
+    //    self.buttonPostPhoto.enabled = YES;
+    //    self.buttonPostStatus.enabled = YES;
+    //    self.buttonPickFriends.enabled = YES;
+    //    self.buttonPickPlace.enabled = YES;
+    
+    // "Post Status" available when logged on and potentially when logged off.  Differentiate in the label.
+    //    [self.buttonPostStatus setTitle:@"Post Status Update (Logged On)" forState:self.buttonPostStatus.state];
+}
+
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user {
+    // here we use helper properties of FBGraphUser to dot-through to first_name and
+    // id properties of the json response from the server; alternatively we could use
+    // NSDictionary methods such as objectForKey to get values from the my json object
+    //    self.labelFirstName.text = [NSString stringWithFormat:@"Hello %@!", user.first_name];
+    // setting the profileID property of the FBProfilePictureView instance
+    // causes the control to fetch and display the profile picture for the user
+    //    self.profilePic.profileID = user.objectID;
+    //    self.loggedInUser = user;
+    //    uilFBUserName.text = [NSString stringWithFormat:@"%@", user.name];
+    NSLog(@"loginViewFetchedUserInfo:(FBLoginView *)loginView");
+    [uilFBUserName setText:[NSString stringWithFormat:@"%@", user.name]];
+    NSLog(@"user.id");
+    PFQuery *query = [PFQuery queryWithClassName:@"pollstartWithGraphPath"];
+    [query whereKey:@"poll" equalTo:@"tpq0***"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            NSLog(@"Successfully retrieved %d scores.", objects.count);
+            [FBRequestConnection startWithGraphPath:@"/me"
+                                         parameters:nil
+                                         HTTPMethod:@"GET"
+                                  completionHandler:^(
+                                                      FBRequestConnection *connection,
+                                                      NSDictionary<FBGraphUser> *nsdMe,
+                                                      NSError *error
+                                                      ) {
+                                      /* handle the result */
+                                      NSLog(@"%@", [nsdMe objectForKey:@"id"]);
+                                  }];
+            // Do something with the found objects
+            for (PFObject *object in objects) {
+                NSLog(@"%@", object.objectId);
+                //                uilMission.text = object.objectId;
+            }
+        } else {
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
+}
+
+//
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    // test to see if we can use the share dialog built into the Facebook application
+    FBLinkShareParams *p = [[FBLinkShareParams alloc] init];
+    p.link = [NSURL URLWithString:@"http://developers.facebook.com/ios"];
+    BOOL canShareFB = [FBDialogs canPresentShareDialogWithParams:p];
+    BOOL canShareiOS6 = [FBDialogs canPresentOSIntegratedShareDialogWithSession:nil];
+    BOOL canShareFBPhoto = [FBDialogs canPresentShareDialogWithPhotos];
+    
+    //    self.buttonPostStatus.enabled = canShareFB || canShareiOS6;
+    //    self.buttonPostPhoto.enabled = canShareFBPhoto;
+    //    self.buttonPickFriends.enabled = NO;
+    //    self.buttonPickPlace.enabled = NO;
+    
+    // "Post Status" available when logged on and potentially when logged off.  Differentiate in the label.
+    //    [self.buttonPostStatus setTitle:@"Post Status Update (Logged Off)" forState:self.buttonPostStatus.state];
+    
+    //    self.profilePic.profileID = nil;
+    //    self.labelFirstName.text = nil;
+    //    self.loggedInUser = nil;
+}
+
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error {
+    // see https://developers.facebook.com/docs/reference/api/errors/ for general guidance on error handling for Facebook API
+    // our policy here is to let the login view handle errors, but to log the results
+    NSLog(@"FBLoginView encountered an error=%@", error);
+}
+
 
 @end
