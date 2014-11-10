73b9bb4905db6313f4bd3d2a7d7024379c688df2
dir <jia-fu.chen@amadeus.com>
2014-11-11 01:05:49 +0800

    [即時戰況] 修正： 1. 所有圖檔下載後皆會暫存本地，下載過的檔案第二次看時不用重新下載 2. 自動判斷圖檔是否已經下載完成，未下載完成的項目不會出現，版面不會顯示錯誤 3. 一次可看所有列表，不需重新整理 4. 處理 UITableViewCell 的 init 及 排版問題

--- ios/view/BLOGTableViewController.m
+++ ios/view/BLOGTableViewController.m
@@ -5,17 +5,26 @@
 //  Created by lololol on 26/Oct/14.
 //  Copyright (c) 2014 appy.tw. All rights reserved.
 //
-
 #import "BLOGTableViewController.h"
 #import "BLOGTableViewCell.h"
 #import "MYParseDelegate.h"
 #import "AppDelegate.h"
 
 #import "SVPullToRefresh.h"
-
-#define LOADING_CELL_TAG 1000
-
 #import "PLISTHeader.h"
+#import "UIImageView+WebCache.h"
+
+@interface BlogItem : NSObject
+@property (nonatomic, strong) NSString *strImageUrl;
+@property (nonatomic, strong) NSString *strSpan;
+@property (nonatomic, strong) NSString *strTime;
+@property (nonatomic, strong) NSString *strLink;
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, unsafe_unretained) CGFloat cellHeight;
+@end
+
+@implementation BlogItem
+@end
 
 @interface BLOGTableViewController ()
 {
@@ -29,23 +38,21 @@
     CGFloat cgfTabBarHeight;
     
     AppDelegate *delegate;
-    
-    NSMutableArray *nsmaImageURL;
-    NSMutableArray *nsmaSpan;
-    NSMutableArray *nsmaTime;
-    NSMutableArray *nsmaLink;
-    NSInteger nsiGlobalMaximum;
-
-    NSMutableArray *nsmaUImage;
-    NSMutableArray *nsmaCellHeight;
-    NSInteger nsiCurrentCellMaximum;
-    BOOL bHasMoreCell;
-    NSString *nssNowTag;
 }
 
+@property (nonatomic, strong) NSMutableArray *blogItemsArr;
 @end
 
 @implementation BLOGTableViewController
+
+- (NSMutableArray*)blogItemsArr
+{
+    if (_blogItemsArr)
+        return _blogItemsArr;
+    
+    self.blogItemsArr = [NSMutableArray array];
+    return _blogItemsArr;
+}
 
 - (void)setMyScreenSize
 {
@@ -65,67 +72,53 @@
 
 - (void)prepareTable
 {
-    _myParseDelegate = [[MYParseDelegate alloc]init];
-    [_myParseDelegate getStart:0];
-    
-    nsmaImageURL = [NSMutableArray new];
-    nsmaLink = [NSMutableArray new];
-    nsmaSpan = [NSMutableArray new];
-    nsmaTime = [NSMutableArray new];
+    MYParseDelegate *parser = [[MYParseDelegate alloc] init];
+    [parser getStart:0];
     
     self.nsmaOutputTable = [NSMutableArray new];
-    nsiGlobalMaximum = [_myParseDelegate.nsmaOutput count];
-    for (NSInteger i = 0; i < nsiGlobalMaximum; i++) {
-        [nsmaTime addObject:[[_myParseDelegate.nsmaOutput objectAtIndex:i]valueForKey:@"pubDate"]];
-        [nsmaSpan addObject:[[_myParseDelegate.nsmaOutput objectAtIndex:i]valueForKey:@"span"]];
-        [nsmaImageURL addObject:[[_myParseDelegate.nsmaOutput objectAtIndex:i]valueForKey:@"img"]];
-        [nsmaLink addObject:[[_myParseDelegate.nsmaOutput objectAtIndex:i]valueForKey:@"link"]];
+
+    for (NSInteger i = 0; i < [parser.nsmaOutput count]; i++) {
+        BlogItem *item = [[BlogItem alloc] init];
+        item.strImageUrl = [[parser.nsmaOutput objectAtIndex:i]valueForKey:@"img"];
+        item.strLink = [[parser.nsmaOutput objectAtIndex:i]valueForKey:@"link"];
+        item.strSpan = [[parser.nsmaOutput objectAtIndex:i]valueForKey:@"span"];
+        item.strTime = [[parser.nsmaOutput objectAtIndex:i]valueForKey:@"pubDate"];
+        [self.blogItemsArr addObject:item];
     }
-    
-//    for (NSInteger i = 0; i < [nsmaLink count]; i++) {
-//        NSLog(@"nsmaTime: %@", [nsmaTime objectAtIndex:i]);
-//        NSLog(@"nsmaSpan: %@", [nsmaSpan objectAtIndex:i]);
-//        NSLog(@"nsmaImageURL: %@", [nsmaImageURL objectAtIndex:i]);
-//        NSLog(@"nsmaLink: %@", [nsmaLink objectAtIndex:i]);
-//    }
-    _myParseDelegate = nil;
 }
 
-- (void)prepareImage:(NSInteger)nsiIndexOfFirstToAdd
-           lastToAdd:(NSInteger)nsiIndexOfLastToAdd{
-//    NSInteger nsiIndexOfFirstToAdd = 0;
-//    NSInteger nsiIndexOfLastToAdd = 1;
-    
-    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-        NSLog(@"nsiIndexOfFirstToAdd: %ld, nsiIndexOfLastToAdd: %ld", (long)nsiIndexOfFirstToAdd, (long)nsiIndexOfLastToAdd);
-        for (NSInteger i = nsiIndexOfFirstToAdd; i <= nsiIndexOfLastToAdd && i <= nsiGlobalMaximum; i ++) {
-            NSLog(@"%ld: %@", (long)i,[NSURL URLWithString:[nsmaImageURL objectAtIndex:i]]);
-            UIImage *uiiTmp = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[nsmaImageURL objectAtIndex:i]]]];
-            [nsmaUImage addObject:uiiTmp];
-            [nsmaCellHeight addObject:[NSString stringWithFormat: @"%.f", cgfScreenWidth * uiiTmp.size.height / uiiTmp.size.width]];
-            NSLog(@"cgfAvailableWidth * uiiTmp.size.height / uiiTmp.size.width: %@", [NSString stringWithFormat: @"%.f", cgfScreenWidth * uiiTmp.size.height / uiiTmp.size.width]);
-            nsiCurrentCellMaximum ++;
-        }
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self.tableView reloadData];
-        });
-    });
+- (void)prepareAllImage
+{
+    [self.blogItemsArr enumerateObjectsUsingBlock:^(BlogItem *item, NSUInteger idx, BOOL *stop) {
+        SDWebImageManager *manager = [SDWebImageManager sharedManager];
+        [manager downloadImageWithURL:[NSURL URLWithString:item.strImageUrl]
+                              options:0
+                             progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+                                 // progression tracking code
+                                 NSLog(@"%ld / %ld", (long)receivedSize, (long)expectedSize);
+                             } completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+                                 if (image && finished)
+                                 {
+                                     // do something with image
+                                     item.image = image;
+                                     item.cellHeight = cgfScreenWidth * image.size.height / image.size.width;
+                                     dispatch_async(dispatch_get_main_queue(), ^{
+                                         [self.tableView reloadData];
+                                     });
+                                 }
+                                 
+                             }];
+        
+    }];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setMyScreenSize];
     [self prepareTable];
+    [self prepareAllImage];
+    
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-    bHasMoreCell = YES;
-    nsiCurrentCellMaximum = 0;
-    nsmaUImage = [NSMutableArray array];
-    nsmaCellHeight = [NSMutableArray array];
-    [self prepareImage:0 lastToAdd:2];
-    [self.tableView addPullToRefreshWithActionHandler:^{
-        [self prepareImage:nsiCurrentCellMaximum + 1 lastToAdd:nsiCurrentCellMaximum + 5];
-        [self.tableView.infiniteScrollingView stopAnimating];
-    } position:SVPullToRefreshPositionBottom];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -139,20 +132,41 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-//    return [_myParseDelegate.nsmaOutput count];
     if (section == 0) {
         return 1;
     }
-    NSLog(@"nsiCurrentCellMaximum: %d", nsiCurrentCellMaximum);
-    return nsiCurrentCellMaximum;
+    return [self.blogItemsArr count];
 }
 
+#define TagImage    55
+#define TagBlack    66
+#define TagSpan     77
+#define TagDate     88
+#define nssGeneralCell  @"GeneralCell"
+
+
+- (UITableViewCell *)createEmptyCell
+{
+    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nssGeneralCell];
+    
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    UIView *blackView = [[UIView alloc] initWithFrame:CGRectZero];
+    UITextView *spanTextView = [[UITextView alloc] initWithFrame:CGRectZero];
+    UITextView *dateTextView = [[UITextView alloc] initWithFrame:CGRectZero];
+    imgView.tag = TagImage;
+    blackView.tag = TagBlack;
+    spanTextView.tag = TagSpan;
+    dateTextView.tag = TagDate;
+    [cell.contentView addSubview:imgView];
+    [cell.contentView addSubview:blackView];
+    [cell.contentView addSubview:spanTextView];
+    [cell.contentView addSubview:dateTextView];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *nssTitleCell = @"TitleCell";
-    static NSString *nssGeneralCell = @"GeneralCell";
-    static NSString *nssMoreCell = @"MoreCell";
     
     if (indexPath.section == 0) {
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:nssTitleCell];
@@ -167,71 +181,61 @@
     }
     
     
-    if (indexPath.row < nsiCurrentCellMaximum) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:nssGeneralCell];
-        if (cell == nil) {
-            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nssGeneralCell];
-        }
-        UIImage *uiiTmp = [nsmaUImage objectAtIndex:indexPath.row];
-        UIImageView *uiivJumbotron = [[UIImageView alloc]initWithFrame:CGRectMake(0.0, 0.0, cgfScreenWidth, cgfScreenWidth * uiiTmp.size.height / uiiTmp.size.width)];
-        uiivJumbotron.image = [nsmaUImage objectAtIndex:indexPath.row];
-        [cell.contentView addSubview:uiivJumbotron];
-        UIView *uivBlackView = [[UIView alloc]initWithFrame:CGRectMake(0.0, [[nsmaCellHeight objectAtIndex:indexPath.row]floatValue] - 49, cgfScreenWidth, 50.0)];
-        [uivBlackView setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.6]];
-        [cell.contentView addSubview:uivBlackView];
-        UITextView *uitvSpan = [[UITextView alloc]initWithFrame:CGRectMake(cgfScreenWidth * 0.04, [[nsmaCellHeight objectAtIndex:indexPath.row]floatValue] - 55, cgfScreenWidth * 0.92, 55.0)];
-        uitvSpan.editable = NO;
-        [uitvSpan setFont:[UIFont systemFontOfSize:12]];
-        [uitvSpan setBackgroundColor:[UIColor clearColor]];
-        [uitvSpan setText:[NSString stringWithFormat:@"%@...", [[nsmaSpan objectAtIndex:indexPath.row]substringToIndex:45]]];
-        [uitvSpan setTextColor:[UIColor whiteColor]];
-        [cell.contentView addSubview:uitvSpan];
-        UITextView *uitvPubDate = [[UITextView alloc]initWithFrame:CGRectMake(cgfScreenWidth * 0.04, [[nsmaCellHeight objectAtIndex:indexPath.row]floatValue] - 25, cgfScreenWidth * 0.90, 35.0)];
-        uitvPubDate.editable = NO;
-        [uitvPubDate setFont:[UIFont systemFontOfSize:12]];
-        [uitvPubDate setBackgroundColor:[UIColor clearColor]];
-        [uitvPubDate setTextAlignment:NSTextAlignmentRight];
-        [uitvPubDate setText:[[nsmaTime objectAtIndex:indexPath.row]substringToIndex:22]];
-        [uitvPubDate setTextColor:[UIColor whiteColor]];
-        [cell.contentView addSubview:uitvPubDate];
-        return cell;
-    } else {
-//        return [self loadingCell];
-        return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:nssGeneralCell];
+    if (cell == nil) {
+        cell = [self createEmptyCell];
     }
     
-//    [cell.uilTitle setText:[[_myParseDelegate.nsmaOutput objectAtIndex:indexPath.row]valueForKey:TAG_TITLE]];
-//    [cell.uitvContent setText:[[_myParseDelegate.nsmaOutput objectAtIndex:indexPath.row]valueForKey:TAG_DESCRIPTION]];
-
+    BlogItem *item = [self.blogItemsArr objectAtIndex:indexPath.row];
     
-//    if (indexPath.row < self.dashboardPosts.count) {
-//        return [self configureCell:indexPath];
-//    } else {
-//        return [self loadingCell];
-//    }
-
+    UIImage *uiiTmp = item.image;
+    if (!uiiTmp)
+        return [self createEmptyCell];
     
+    
+    UIImageView *uiivJumbotron = (UIImageView*)[cell.contentView viewWithTag:TagImage];
+    uiivJumbotron.frame = CGRectMake(0.0, 0.0, cgfScreenWidth, cgfScreenWidth * uiiTmp.size.height / uiiTmp.size.width);
+    uiivJumbotron.image = uiiTmp;
+    
+    UIView *uivBlackView = [cell.contentView viewWithTag:TagBlack];
+    uivBlackView.frame = CGRectMake(0.0, item.cellHeight - 49, cgfScreenWidth, 50.0);
+    [uivBlackView setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.6]];
+    
+    UITextView *uitvSpan = (UITextView*)[cell.contentView viewWithTag:TagSpan];
+    uitvSpan.frame =CGRectMake(cgfScreenWidth * 0.04, item.cellHeight - 55, cgfScreenWidth * 0.92, 55.0);
+    uitvSpan.editable = NO;
+    [uitvSpan setFont:[UIFont systemFontOfSize:12]];
+    [uitvSpan setBackgroundColor:[UIColor clearColor]];
+    [uitvSpan setText:[NSString stringWithFormat:@"%@...", [item.strSpan substringToIndex:45]]];
+    [uitvSpan setTextColor:[UIColor whiteColor]];
+    
+    UITextView *uitvPubDate = (UITextView*)[cell.contentView viewWithTag:TagDate];
+    uitvPubDate.frame = CGRectMake(cgfScreenWidth * 0.04, item.cellHeight - 25, cgfScreenWidth * 0.90, 35.0);
+    uitvPubDate.editable = NO;
+    [uitvPubDate setFont:[UIFont systemFontOfSize:12]];
+    [uitvPubDate setBackgroundColor:[UIColor clearColor]];
+    [uitvPubDate setTextAlignment:NSTextAlignmentRight];
+    [uitvPubDate setText:[item.strTime substringToIndex:22]];
+    [uitvPubDate setTextColor:[UIColor whiteColor]];
+    
+    return cell;
 }
 
 #pragma mark - Table view delegate
-
-//- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-//    if (cell.tag == LOADING_CELL_TAG) {
-//        cell.backgroundColor = [UIColor colorWithRed:240.0/255.0 green:240.0/255.0 blue:240.0/255.0 alpha:1.0];
-//        if (bHasMoreCell == YES) {
-//            [self requestDashboardPosts];
-//        }
-//    }
-//}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.section == 0) {
         return cgfScreenHeightBase + self.tableView.frame.size.width * 45 / 320 + cgfScreenHeight * 0.04;
     } else {
-        return [[nsmaCellHeight objectAtIndex:indexPath.row]floatValue] + 10;
+        BlogItem *item = [self.blogItemsArr objectAtIndex:indexPath.row];
+        if (item.image)
+            return item.cellHeight + 10;
+        else
+            return 0;       // image has not downloaded yet.
     }
 }
+
 
 /*
 // Override to support conditional editing of the table view.
@@ -277,21 +281,5 @@
 }
 */
 
-- (UITableViewCell *)loadingCell {
-    UITableViewCell *cell = [[UITableViewCell alloc]
-                             initWithStyle:UITableViewCellStyleDefault
-                             reuseIdentifier:nil];
-    
-    UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc]
-                                                  initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-    activityIndicator.center = cell.center;
-    [cell.contentView addSubview:activityIndicator];
-    
-    [activityIndicator startAnimating];
-    
-    cell.tag = LOADING_CELL_TAG;
-    
-    return cell;
-}
 
 @end
