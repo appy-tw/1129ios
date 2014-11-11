839681260fe77074355671bc6b996f5bfefe03fa
AWS Server <668jerry@gmail.com>
2014-11-12 04:25:41 +0800

    blogviewcontroller, supviewcontroller, and attviewcontroller are added

---
+++ ios/view/ATTViewController.m
@@ -0,0 +1,37 @@
+//
+//  ATTViewController.m
+//  ios
+//
+//  Created by lololol on 12/Nov/14.
+//  Copyright (c) 2014 Appendectomy Project. All rights reserved.
+//
+
+#import "ATTViewController.h"
+
+@interface ATTViewController ()
+
+@end
+
+@implementation ATTViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ ios/view/BLOGViewController.m
@@ -0,0 +1,37 @@
+//
+//  BLOGViewController.m
+//  ios
+//
+//  Created by lololol on 12/Nov/14.
+//  Copyright (c) 2014 Appendectomy Project. All rights reserved.
+//
+
+#import "BLOGViewController.h"
+
+@interface BLOGViewController ()
+
+@end
+
+@implementation BLOGViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
